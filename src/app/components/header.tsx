37d204e82111162b2915fba496443e784965cf2c
SirJavaDrinker <trichardpowell@gmail.com>
2025-10-06 21:47:46 -0400

    refactor useRouter

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import Dropdown from "./dropdown-nav";
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className="bg-stone-200 text-black p-4 text-center border-b-2 border-stone-300 flex items-center sticky top-0 z-50">
       <div className="w-1/3 text-left">
@@ -15,7 +16,6 @@ export default function Header() {
             ]
           }
           onSelect={(value) => {
-            const router = useRouter();
             if (value=="nav-home") {
               router.push('/');
             } else if (value=="nav-rules") {
